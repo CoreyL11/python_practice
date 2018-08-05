adb04c5ff9da293063eae52a5eb23bae81089545
CoreyL11 <41996984+CoreyL11@users.noreply.github.com>
2018-08-05 16:49:24 +0100

    Update and rename slicing_strings_1.py to slicing_strings.py

--- slicing_strings.py
+++ slicing_strings.py
@@ -6,9 +6,12 @@ data.index('|')
 
 #variable 'product' is equal to variable 'data', [: is saying anything before the colon you want up to data.index('|')] so this will go "'Playstation 4 Pro' |"
 product = data[:data.index('|')]
-price = data[18:26]
+price = data[20:25]
 condition = data[27:]
 
-#print (product,price,condition)
+print(product)
+print(price)
+print(condition)
 
-print(data[data.find('|'):data.find('|', 18)])
+## Not sure what the below is meant to do
+#print(data[data.find('|'):data.find('|', 18)])
